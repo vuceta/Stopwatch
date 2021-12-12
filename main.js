0c4ff29021b6469f8258f09585fea7437fab4c3b
vuceta <vuceta.obradovic@aiesec.net>
2021-12-12 13:35:52 +0100

    finished

---
+++ main.js
@@ -0,0 +1,63 @@
+let playBtn = document.getElementById("playBtn");
+let pauseBtn = document.getElementById("pauseBtn");
+let resetBtn = document.getElementById("resetBtn");
+
+playBtn.addEventListener("click", start);
+pauseBtn.addEventListener("click", pause);
+resetBtn.addEventListener("click", reset);
+
+let startTime;
+let elapsedTime = 0;
+let timerInterval;
+
+function print(txt) {
+  document.getElementById("display").innerHTML = txt;
+}
+
+function timeToString(time) {
+  let diffInHrs = time / 3600000;
+  let hh = Math.floor(diffInHrs);
+
+  let diffInMin = (diffInHrs - hh) * 60;
+  let mm = Math.floor(diffInMin);
+
+  let diffInSec = (diffInMin - mm) * 60;
+  let ss = Math.floor(diffInSec);
+
+  let diffInMs = (diffInSec - ss) * 1000;
+  let ms = Math.floor(diffInMs);
+
+  let formattedHH = mm.toString().padStart(2, "0");
+  let formattedMM = ss.toString().padStart(2, "0");
+  let formattedSS = ms.toString().padStart(2, "0");
+
+  return `${formattedHH}:${formattedMM}:${formattedSS}`;
+}
+
+function start() {
+  startTime = Date.now() - elapsedTime;
+  timerInterval = setInterval(function printTime() {
+    elapsedTime = Date.now() - startTime;
+    print(timeToString(elapsedTime));
+  }, 10);
+  showBtn("PAUSE");
+}
+
+function pause() {
+  clearInterval(timerInterval);
+  showBtn("PLAY");
+}
+
+function reset() {
+  clearInterval(timerInterval);
+  print("00:00:000");
+  elapsedTime = 0;
+  showBtn("PLAY");
+}
+
+function showBtn(btnKey) {
+  const btnToShow = btnKey === "PLAY" ? playBtn : pauseBtn;
+  const btnToHide = btnKey === "PLAY" ? pauseBtn : playBtn;
+  btnToShow.style.display = "block";
+  btnToHide.style.display = "none";
+}
